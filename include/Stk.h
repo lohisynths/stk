e536bb4cf63e53a9c1fb5088d7a4c80cdbb6372b
lohisynths <lohisynths@gmail.com>
2017-05-28 16:51:20 +0200

    fast_sin added

--- include/Stk.h
+++ include/Stk.h
@@ -596,9 +596,53 @@ namespace stk
 namespace math
 {
 
+
+union el
+{
+	StkFloat z1;
+	int z2;
+};
+
+inline StkFloat fast_sin(StkFloat x) {
+	int k;
+	StkFloat y;
+	StkFloat z;
+
+	el zz;
+
+	z  = x;
+	z *= 0.3183098861837907;
+	z += 6755399441055744.0;
+
+	zz.z1 = z;
+
+	//k  = *((int *) &z);
+	k = zz.z2;
+
+	z  = k;
+	z *= 3.1415926535897932;
+	x -= z;
+	y  = x;
+	y *= x;
+	z  = 0.0073524681968701;
+	z *= y;
+	z -= 0.1652891139701474;
+	z *= y;
+	z += 0.9996919862959676;
+	x *= z;
+	k &= 1;
+	k += k;
+	z  = k;
+	z *= x;
+	x -= z;
+
+	return x;
+}
+
 inline StkFloat sin(StkFloat x)
 {
-	return std::sin((long double)x);
+	return fast_sin(x);
+	//return std::sin((long double)x);
 }
 inline StkFloat cos(StkFloat x)
 {
